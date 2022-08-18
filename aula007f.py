1a7c8f7981d15ae8fce32677991cd2cc691d8afd
Daniela Rocha Porto Ikeda <111469110+daniikeda@users.noreply.github.com>
2022-08-17 21:58:59 -0300

    Add files via upload

---
+++ aula007f.py
@@ -0,0 +1,25 @@
+#O objetivo desse exercício é construir uma tabuada de acordo com o número inteiro solicitado pelo usuário
+num1: int=int(input('Digite o número que deseja obter a tabuada:'))
+tabu1=num1*1
+tabu2=num1*2
+tabu3=num1*3
+tabu4=num1*4
+tabu5=num1*5
+tabu6=num1*6
+tabu7=num1*7
+tabu8=num1*8
+tabu9=num1*9
+tabu10=num1*10
+print('A tabuada de {} é:'.format(num1))
+print('{} x 1= {}'.format(num1, tabu1))
+print('{} x 2= {}'.format(num1, tabu2))
+print('{} x 3= {}'.format(num1, tabu3))
+print('{} x 4= {}'.format(num1, tabu4))
+print('{} x 5= {}'.format(num1, tabu5))
+print('{} x 6= {}'.format(num1, tabu6))
+print('{} x 7= {}'.format(num1, tabu7))
+print('{} x 8= {}'.format(num1, tabu8))
+print('{} x 9= {}'.format(num1, tabu9))
+print('{} x 10= {}'.format(num1, tabu10))
+
+
